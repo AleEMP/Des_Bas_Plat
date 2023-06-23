2072e5026ab794ec9c6cf54367ef6ac61cb9c9d8
AleEMP <aleja.emp11@gmail.com>
2023-06-23 12:39:04 -0500

    Xamarin

---
+++ Form CV Xamarin/UnU/UnU/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using UnU.View;
+using Xamarin.Forms.Xaml;
+
+namespace UnU
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new PrimeraPag());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
